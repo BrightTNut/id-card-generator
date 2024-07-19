11cfd61ea058c712e5d494d7805644ec48b6bf90
BrightTNut <118769123+BrightTNut@users.noreply.github.com>
2024-07-19 17:51:43 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -7,7 +7,9 @@ const app = express();
 app.use(express.json());
 app.use(
   cors({
-    origin: "https://id-card-generator-pi.vercel.app/",
+    origin: ["https://deploy-mern-1whq.vercel.app"],
+    methods: ["POST", "GET"],
+    credentials: true,
   })
 );
 
